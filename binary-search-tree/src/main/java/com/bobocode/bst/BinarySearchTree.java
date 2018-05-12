27fe689d076af5e153e255b5e839493f2fcb3927
Taras <boy4uck@gmail.com>
2018-05-12 19:19:14 +0300

    added short description

--- binary-search-tree/src/main/java/com/bobocode/bst/BinarySearchTree.java
+++ binary-search-tree/src/main/java/com/bobocode/bst/BinarySearchTree.java
@@ -1,4 +1,12 @@
 package com.bobocode.bst;
 
+/**
+ * Binary search tree should have an API that allow provides the following functionality:
+ * - insert an element and return true if it was inserted successfully
+ * - search an element and return true it element exists
+ * - get tree size
+ * - get tree height
+ * - perform in-order traversal by passing element @{@link java.util.function.Consumer} as a parameter
+ */
 public interface BinarySearchTree {
 }
