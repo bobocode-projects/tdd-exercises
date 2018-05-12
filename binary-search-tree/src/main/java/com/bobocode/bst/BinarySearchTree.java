7b3241a66ab5e399464877e957ec8c642f283b30
Taras <boy4uck@gmail.com>
2018-05-12 15:35:03 +0300

    - removed interface methods - added hamcrest dependency - added test class

--- binary-search-tree/src/main/java/com/bobocode/bst/BinarySearchTree.java
+++ binary-search-tree/src/main/java/com/bobocode/bst/BinarySearchTree.java
@@ -1,17 +1,4 @@
 package com.bobocode.bst;
 
-import java.util.List;
-
-public interface BinarySearchTree<T extends Comparable> {
-    boolean insert(T element);
-
-    int size();
-
-    int height();
-
-    List<T> inOrderTraversal();
-
-    List<T> preOrderTraversal();
-
-    List<T> postOrderTraversal();
+public interface BinarySearchTree {
 }
---
+++ binary-search-tree/src/test/java/com/bobocode/bst/BinarySearchTreeTest.java
@@ -0,0 +1,9 @@
+package com.bobocode.bst;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class BinarySearchTreeTest {
+
+}
