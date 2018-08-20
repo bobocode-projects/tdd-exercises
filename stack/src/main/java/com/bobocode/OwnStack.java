ecfb81477abe403d6426552c540080834f614e07
v.radchenko <v.radchenko@yaypay.com>
2018-08-20 12:56:53 +0300

    done empty stack

--- stack/src/main/java/com/bobocode/OwnStack.java
+++ stack/src/main/java/com/bobocode/OwnStack.java
@@ -1,39 +1,62 @@
 package com.bobocode;
 
-public class OwnStack<T> implements Stack<T> {
+import java.util.EmptyStackException;
+import java.util.Objects;
 
+public class OwnStack<T> implements Stack<T> {
     private static class Node<T> {
         private T element;
-        private Node<T> next;
+        private Node<T> prev;
 
         private Node(T element) {
             this.element = element;
         }
 
-        public static Node<T> valueOf(T element) {
+        static <T> Node<T> valueOf(T element) {
             return new Node<>(element);
         }
     }
+
+    private int size;
 
     private Node<T> head;
 
     @Override
     public void push(T element) {
+        if (Objects.isNull(element)){
+            return;
+        }
+        Node<T> newNode = Node.valueOf(element);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.prev = head;
+            head = newNode;
+        }
 
+        size++;
     }
 
     @Override
     public T pop() {
-        return null;
+        if (Objects.isNull(head)) {
+            throw new EmptyStackException();
+        }
+
+        T element = head.element;
+        head = head.prev;
+        size--;
+
+        return element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head == null;
     }
 }
--- stack/src/test/java/com/bobocode/StackTest.java
+++ stack/src/test/java/com/bobocode/StackTest.java
@@ -1,9 +1,94 @@
 package com.bobocode;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.EmptyStackException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(JUnit4.class)
 public class StackTest {
     private Stack<Integer> stack = new OwnStack<>();
+
+    @Test
+    public void validateEmptyMethod() {
+        stack.push(12);
+        stack.push(14);
+        stack.push(16);
+
+        assertThat("Stack should not be empty", !stack.isEmpty());
+    }
+
+    @Test
+    public void validateSizeAddingMethod() {
+        stack.push(12);
+        stack.push(14);
+
+        assertThat("Size method is wrong", stack.size(), equalTo(2));
+    }
+
+    @Test
+    public void validateSizeDeletingMethod() {
+        stack.push(12);
+        stack.push(14);
+        stack.pop();
+
+        assertThat("Size method is wrong", stack.size(), equalTo(1));
+    }
+
+    @Test
+    public void validatePushWithOneElementMethod() {
+        stack.push(12);
+
+        Integer element = stack.pop();
+        assertThat("Wrong number", element, equalTo(12));
+    }
+
+    @Test
+    public void validatePushWithManyAddingMethod() {
+        stack.push(12);
+        stack.push(14);
+        stack.push(16);
+
+        Integer element = stack.pop();
+        assertThat("Wrong number", element, equalTo(16));
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void validatePopEmptyStack() {
+        stack.pop();
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void validatePopWithNotEmptyStack() {
+        stack.push(12);
+        stack.push(10);
+        stack.pop();
+        stack.pop();
+        stack.pop();
+    }
+
+    @Test
+    public void validatePushWithEmptyValuesForEmptyStack() {
+        stack.push(null);
+        stack.push(null);
+        stack.push(null);
+
+        assertThat("Stack should be empty", stack.isEmpty());
+        assertThat("Size for pushed null values should be 0", stack.size(), equalTo(0));
+    }
+
+    @Test
+    public void validatePushWithEmptyValues() {
+        stack.push(12);
+        stack.push(10);
+        stack.push(null);
+
+        assertThat("Stack should not be empty", !stack.isEmpty());
+        assertThat("Size for pushed null values should be 0", stack.size(), equalTo(2));
+    }
+
 }
